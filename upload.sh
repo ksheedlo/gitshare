c3d0d36fb736206ae612c11f2bb962662dac4a3f
Ken Sheedlo <ovrkenthousand@gmail.com>
2013-06-23 12:24:20 -0700

    Add quick upload script

---
+++ upload.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+if [[ -z "$1" ]]
+then
+    cat <<EOF
+Usage: $0 [FILE]
+    FILE is the file to be uploaded
+EOF
+    exit 1
+fi
+
+filename=$(basename "$1")
+cp "$1" ".data/$filename"
+echo "[$filename](https://raw.github.com/ksheedlo/gitshare/master/.data/$filename)]" >>README.md
+git add ".data/$filename"
+git add README.md
+git ci -m "Upload $filename"
+git push
+
